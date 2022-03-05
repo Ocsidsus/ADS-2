5868d327acb142dcd5b160b17edd94d5e29f9f1a
Dmitry Volkov <83473625+Ocsidsus@users.noreply.github.com>
2022-03-05 15:00:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,22 +22,24 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    if (count == 1) {
+    if (count == 0) {
         return 1;
     }
-    return calcItem(x, count - 1) + expn(x, count - 1);
+    return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
     if (count == 1) {
         return x;
     }
-    return calcItem(x, 2 * count - 1) * pown(-1, count - 1) + sinn(x, count - 1);
+    return calcItem(x, 2 * count - 1) * pown(-1, count - 1) + 
+    sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
     if (count == 1) {
         return 1;
     }
-    return calcItem(x, 2 * count - 2) * pown(-1, count - 1) + cosn(x, count - 1);
+    return calcItem(x, 2 * count - 2) * pown(-1, count - 1) + 
+    cosn(x, count - 1);
 }
